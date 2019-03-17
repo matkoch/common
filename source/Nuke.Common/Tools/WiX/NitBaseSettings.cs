d5a8a037f739ec72b645cea0a651140704bd2039
Jonas Stjernström <jonas.stjernstrom@nethouse.se>
2019-03-17 23:22:44 +0100

    Adds Wix support

---
+++ source/Nuke.Common/Tools/WiX/NitBaseSettings.cs
@@ -0,0 +1,19 @@
+// Copyright 2019 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System;
+using Nuke.Common.Tooling;
+
+namespace Nuke.Common.Tools.Wix
+{
+    public partial class NitBaseSettings
+    {
+        public override Action<OutputType, string> CustomLogger { get; }
+        
+        public string GetToolPath()
+        {
+            return $@"{EnvironmentInfo.Variable("WIX")}\bin\nit.exe";
+        }
+    }
+}
---
+++ source/Nuke.Common/Tools/WiX/WixTasks.cs
@@ -0,0 +1,14 @@
+// Copyright 2019 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+namespace Nuke.Common.Tools.Wix
+{
+    public partial class WixTasks
+    {
+        private static string GetToolPath()
+        {
+            return null;
+        }
+    }
+}
