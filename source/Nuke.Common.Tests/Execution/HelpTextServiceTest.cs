9699a65ff02c07c8b50afb44724b97a337e505eb
Volkmar Rigo <volkmar@rigo.bz>
2019-05-05 13:39:42 +0200

    Changed combinded test to separate tests.

--- source/Nuke.Common.Tests/Execution/HelpTextServiceTest.cs
+++ source/Nuke.Common.Tests/Execution/HelpTextServiceTest.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 using FluentAssertions;
+using System.Linq;
 
 namespace Nuke.Common.Tests.Execution {
     public class HelpTextServiceTest {
@@ -15,14 +16,33 @@ namespace Nuke.Common.Tests.Execution {
             }
         }
 
+        private void ParameterHelpText(NukeBuild build, string expectedParamName, string expectedHelpText) {
+
+            var helpTextLine = HelpTextService.GetParametersText(build).Split(Environment.NewLine + "  --").FirstOrDefault(q => q.StartsWith(expectedParamName));
+            if (!string.IsNullOrEmpty(helpTextLine))
+                helpTextLine = helpTextLine.Split(" ", 2)[1].Replace(Environment.NewLine, " ").Trim();
+            helpTextLine.Should().EndWith(expectedHelpText);
+        }
+
+
         private class StringParamWithDescriptionBuild : DummyBuild {
             [Parameter("TestDescription")]
             private readonly string TestParam;
         }
 
+        [Fact]
+        public void StringParamWithDescription() {
+            ParameterHelpText(new StringParamWithDescriptionBuild(), "test-param", "TestDescription.");
+        }
+
         private class StringParamWithDefaultValueBuild : DummyBuild {
             [Parameter()]
             private readonly string StringParam = "TEST";
+        }
+
+        [Fact]
+        public void StringParamWithDefaultValue() {
+            ParameterHelpText(new StringParamWithDefaultValueBuild(), "string-param", "The Default Value is \"TEST\".");
         }
 
         private class EnumParamWithDefaultValueBuild : DummyBuild {
@@ -31,22 +51,9 @@ namespace Nuke.Common.Tests.Execution {
             private readonly TestEnum EnumParam = TestEnum.Maybe;
         }
 
-
-        public static IEnumerable<object[]> Data =>
-                new List<object[]>
-                    {
-                        new object[] { new StringParamWithDescriptionBuild(),  "  --test-param         TestDescription." },
-                        new object[] { new StringParamWithDefaultValueBuild(), "  --string-param       The Default Value is \"TEST\"." },
-                        new object[] { new EnumParamWithDefaultValueBuild(),   "  --enum-param         The available Values are \"Yes\", \"No\", \"Maybe\". The Default Value is \"Maybe\"." },
-                    };
-
-        [Theory]
-        [MemberData(nameof(Data))]
-        public void ParameterHelpText(NukeBuild build, string expectedHelpText) {
-
-            HelpTextService.GetParametersText(build).
-                Split(Environment.NewLine).
-                Should().Contain(expectedHelpText);
+        [Fact]
+        public void EnumParamWithDefaultValue() {
+            ParameterHelpText(new EnumParamWithDefaultValueBuild(), "enum-param", "The available Values are \"Yes\", \"No\", \"Maybe\". The Default Value is \"Maybe\".");
         }
 
     }
