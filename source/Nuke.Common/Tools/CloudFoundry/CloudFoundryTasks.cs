b78b07c33331df1e82744af944a89fa1c82c578a
Andrew Stakhov <andrew.stakhov@gmail.com>
2019-05-22 21:55:29 -0400

    Cloud foundry specification

---
+++ source/Nuke.Common/Tools/CloudFoundry/CloudFoundryTasks.cs
@@ -0,0 +1,32 @@
+// Copyright 2019 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Nuke.Common.Tools.CloudFoundry
+{
+    public static partial class CloudFoundryTasks
+    {
+        public static async Task CloudFoundryEnsureServiceReady(string serviceInstance)
+        {
+            bool IsCreating()
+            {    
+                var commandResult = CloudFoundryGetServiceInfo(c => c
+                        .SetServiceInstance(serviceInstance))
+                    .Where(x => x.Text.StartsWith("status:"))
+                    .First()
+                    .Text;
+                var result = Regex.Match(commandResult, @"(?<=^status:\s+)[a-z].+", RegexOptions.Multiline).Value;
+                return result == "create in progress";
+            }
+            while (IsCreating())
+            {
+                await Task.Delay(5000);
+            }
+            
+        }
+    }
+}
